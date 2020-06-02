b4ffdc09ee1d86aaecdbf2a8477674e4de796b36
KLTKuroi <joandelcanto99@gmail.com>
2020-06-02 18:58:37 +0200

    Updates

---
+++ precache-manifest.b9a67cfa4c78e0a8d3ba32e701a4aca2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f438e8427ccb706fb05166b130178d13",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "4a3f687227056fd8e575",
+    "url": "/monsters-rolodex/static/css/main.7571af1d.chunk.css"
+  },
+  {
+    "revision": "cae8900dcf0593a9e8f0",
+    "url": "/monsters-rolodex/static/js/2.975f868f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.975f868f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a3f687227056fd8e575",
+    "url": "/monsters-rolodex/static/js/main.da8dc236.chunk.js"
+  },
+  {
+    "revision": "e3f9453037e5f28f8167",
+    "url": "/monsters-rolodex/static/js/runtime-main.7c0f1f0f.js"
+  }
+]);
